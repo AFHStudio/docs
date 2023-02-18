2b98eaa8300c66c692ee7b6b95cd20a8487d4584
Akshayan <68979357+akshayans@users.noreply.github.com>
2023-02-18 10:23:37 -0500

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,5 @@
 import { defaultTheme } from '@vuepress/theme-default';
 import { searchPlugin } from '@vuepress/plugin-search';
-import { contributors } from '@kidonng/vuepress-plugin-contributors'
 export default {
   theme: defaultTheme({
     logo: '/docslogo.png',
@@ -25,12 +24,5 @@ export default {
         }
       }
     }),
-    contributors({
-      docsRepo: 'afhstudio/docs',
-      docsBranch: 'main',
-      docsDir: 'docs',
-      label: 'Contributors',
-      api: 'https://api.xuann.wang/api/github-file-contributors'
-    })
   ]
 }
