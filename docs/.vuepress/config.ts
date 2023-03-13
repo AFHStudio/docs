0f93d1a2979079c317f5b28f9302196095b2cea7
Akshayan <68979357+akshayans@users.noreply.github.com>
2023-03-12 23:16:23 -0400

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default {
         },
         {
           text: 'Deck Docs',
-              children: ['/guides/deck/deckgames.md', '/guides/deck/README.md'],
+              children: ['/guides/deck/README.md', '/guides/deck/deckgames.md'],
         },
       ],
   }),
